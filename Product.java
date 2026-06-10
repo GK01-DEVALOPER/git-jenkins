fdb7bc58f7680f70dfa419040c83c867d097db3e
root <root@ip-172-31-27-148.ec2.internal>
2026-06-10 06:36:41 +0000

    multiplication

---
+++ Product.java
@@ -0,0 +1,13 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        // Define two integer variables
+        int number1 = 5;
+        int number2 = 10;
+        
+        // Calculate the product using the * operator
+        int product = number1 * number2;
+        
+        // Output the final result to the console
+        System.out.println("The product is: " + product);
+    }
+}
